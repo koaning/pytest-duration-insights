966ed4b865244a6cc96a5db5aab51e6d12b13282
koaning <vincentwarmerdam@gmail.com>
2021-06-04 21:58:33 +0200

    itworks

--- pytest-duration-insights/__main__.py
+++ pytest-duration-insights/__main__.py
@@ -6,12 +6,12 @@ from pathlib import Path
 
 from clumper import Clumper 
 
-from ._parsing import to_hierarchy_dict, parse_test_info
+from ._parsing import to_hierarchy_dict, parse_test_info, Node
 
 
 if __name__ == "__main__":
     """
-    pytest-duration-insights --report-file --open-browser --reduce
+    pytest-duration-insights --report-file --open-browser --reduce --port
     """
     report_path = sys.argv[1]
     res = (Clumper.read_jsonl(report_path)
@@ -21,7 +21,11 @@ if __name__ == "__main__":
     tmpdir = tempfile.mkdtemp()
     shutil.copytree(src=Path(__file__).parent / "static", 
                     dst=Path(tmpdir) / "static")
-    Clumper(res, listify=False).write_json(Path(tmpdir) / "static" / "data.json")
+    
+    data_path = Path(tmpdir) / "static" / "data.json"
+    tree_path = Path(tmpdir) / "static" / "treedata.json"
+    Clumper(res, listify=False).write_json(data_path)
+    Clumper(Node.from_dict(res).to_value_dict(), listify=False).write_json(tree_path)
 
     import subprocess
     
