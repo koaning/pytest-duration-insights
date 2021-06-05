cf942ffe8e7956a2b09cf9ca6d579ad5f5e5a93f
Vincent <vincentwarmerdam@gmail.com>
2021-06-05 16:56:43 +0200

    added-typer

---
+++ pytest_duration_insights/__main__.py
@@ -0,0 +1,58 @@
+import sys
+import typer
+import shutil
+import tempfile
+import subprocess
+from pathlib import Path
+
+from clumper import Clumper 
+
+from ._parsing import to_hierarchy_dict, parse_test_info
+from pytest_duration_insights import VERSION
+
+
+app = typer.Typer(
+    name="pytest duration insights", 
+    add_completion=False, 
+    help="""
+    This app allows you to inspect and explore pytest run times.
+
+    Assuming you've got `pytest-reportlog` installed you can collect
+    test run data and visualise it via;
+
+    pytest --report-log reportlog.jsonl
+
+    pytest-duration-insights explore reportlog.jsonl
+    """
+)
+
+@app.command()
+def explore(report_path: str = typer.Argument(..., help="Report log to visualise."), 
+            no_trim: bool = typer.Option(False, is_flag=True, help="Flag to not reduce parametrized calls."), 
+            port: int = typer.Option(8002, help="Port to serve the dashboard on.")):
+    """Starts up a pytest reportlog dashboard."""
+    # Use clumper to arrange the data appropriately.
+    res = (Clumper.read_jsonl(report_path)
+              .pipe(parse_test_info, trim=not no_trim)
+              .pipe(to_hierarchy_dict, hierarchy_col="hierarchy", value_col="duration"))
+    
+    # We server everything as static files from a temporary folder
+    tmpdir = tempfile.mkdtemp()
+    shutil.copytree(src=Path(__file__).parent / "static", 
+                    dst=Path(tmpdir) / "static")
+    Clumper(res, listify=False).write_json(Path(tmpdir) / "static" / "data.json")
+    
+    # This a bit hacky but does the job
+    subprocess.run(["python", "-m", "http.server", str(port), "--directory", str(Path(tmpdir) / "static")])
+
+@app.command()
+def version():
+    """Returns the version."""
+    return VERSION
+       
+
+if __name__ == "__main__":
+    """
+    pytest-duration-insights --report-file --open-browser --reduce
+    """
+    app()
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
+from pytest_duration_insights import VERSION
 
 base_packages = ["pytest-reportlog", "clumper", "parse"]
 
 
 setup(
     name="pytest-duration-insights",
-    version="0.0.1",
+    version=VERSION,
     packages=find_packages(exclude=['notebooks']),
     install_requires=base_packages,
 )
