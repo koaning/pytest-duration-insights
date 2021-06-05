92d25c54ac07a2e22a73387a2520aa35da127eb6
Vincent <vincentwarmerdam@gmail.com>
2021-06-05 17:18:59 +0200

    percentages

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 web:
 	cd svelte-app; npm run build
-	cp -r svelte-app/public/* pytest-duration-insights/static
+	cp -r svelte-app/public/* pytest_duration_insights/static
 
 demo: web
-	python -m pytest-duration-insights tests/data/scikit-lego-reportlog.jsonl
+	python -m pytest_duration_insights explore tests/data/scikit-lego-reportlog.jsonl
--- svelte-app/src/Tree.svelte
+++ svelte-app/src/Tree.svelte
@@ -1,12 +1,12 @@
 <script>
     export let data;
     export let expanded = false;
+    export let maxtime = data['value'];
     let color = "black";
     function toggle(){
         expanded = !expanded;
         color = "lightgray" ? expanded : "black"
     }
-    console.log(data);
 </script>
 
 <style>
@@ -18,16 +18,16 @@
 }
 </style>
 
-<p class="{color}" on:click={toggle}>{data['name']} - {data['value']}</p>
+<p class="{color}" on:click={toggle}>{data['name']} - {data['value']} - {Math.round(data['value']/maxtime*10000)/100}%</p>
 <p style="padding-left: 30px">
     {#each data['children'] as child}
         {#if 'children' in child}
             {#if expanded}
-                <span><svelte:self data={child}/></span>
+                <span><svelte:self data={child} maxtime={maxtime}/></span>
             {/if}    
         {:else}
             {#if expanded}
-                <p>{child['name']} - {child['value']}</p>
+                <p>{child['name']} - {child['value']} - {Math.round(data['value']/maxtime*10000)/100}%</p>
             {/if}
         {/if}
     {/each}
