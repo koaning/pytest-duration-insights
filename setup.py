c53b9721c8a5b50f0eaacce7d9663e66a61028cc
Vincent <vincentwarmerdam@gmail.com>
2021-06-18 15:20:02 +0200

    nearly ready for live

--- setup.py
+++ setup.py
@@ -28,9 +28,18 @@ setup(
     },
     package_data={"pytest_duration_insights": ["static/*", "static/**/*"]},
     long_description=read("readme.md"),
+    long_description_content_type="text/markdown",
     project_urls={
         "Bug Tracker": "https://github.com/koaning/pytest-duration-insights/issues",
         "Documentation": "https://github.com/koaning/pytest-duration-insights",
         "Source Code": "https://github.com/koaning/pytest-duration-insights",
     },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
