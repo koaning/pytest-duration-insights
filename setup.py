b38b03f608b2699c229dadf772b72f65b42af7a2
vincent d warmerdam <vincentwarmerdam@gmail.com>
2021-06-27 19:32:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,9 +37,9 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
     ],
 )
